a9fc45fa188a2ae27730b55111598821d7cbbe56
BarnetteME1 <BarnetteME1@gmail.com>
2015-10-29 11:56:38 -0400

    added some views

--- restapp/views.py
+++ restapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView, View
+
+
+class RestaurauntList(ListView):
+    pass
+
+
+class MenuList(ListView):
+    pass
+
